70c3664097b2bf661f809720be681cac9c81d8fe
Nikita Mykhailov <nikmikhailov13@gmail.com>
2026-04-15 17:47:16 +0200

    add string samples and shortcut comments

---
+++ src/lesson_7_string/Main.java
@@ -0,0 +1,27 @@
+package lesson_7_string;
+
+public class Main {
+
+    void main() {
+        // create a string with constructor
+        String s1 = "Hello";
+
+        System.out.println(s1);
+
+        // preferred approach: create a string with =
+        String s2 = "world";
+        System.out.println(s2);
+
+        // string concatenation
+        String s3 = s1 + " " + s2;
+        System.out.println(s3);
+        System.out.println(s3);
+
+        // for Windows: command - ctrl, option - alt
+
+        // option + command + L - форматування коду
+        // option + command + shift + L - розширене форматування коду
+        // command + D - дублювання рядку
+        // command + shift + стілочка вгору/вниз - переміщення рядків
+    }
+}
