62b87e8ad6fec5c68fe788bddcdc54f0940b3fe4
Nikita Mykhailov <nikmikhailov13@gmail.com>
2026-04-24 17:53:48 +0200

    add break and continue

--- src/lesson_12_loops/Main.java
+++ src/lesson_12_loops/Main.java
@@ -23,5 +23,36 @@ public class Main {
         for (int i = 10; i <= 15; i++) {
             System.out.println("Значення i: " + i);
         }
+
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // classic for loop + break
+        System.out.println("Classic for loop + break");
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] == 30){
+                break;
+            }
+            System.out.println(numbers[i]);
+        }
+
+        // for each + break
+        System.out.println("For each + break");
+        for (int n : numbers) {
+            if (n == 30) {
+                break;
+            }
+            System.out.println(n);
+        }
+
+        // for each + continue
+        System.out.println("Fore each + continue");
+        for (int n : numbers) {
+            if (n == 30) {
+                continue;
+            }
+            System.out.println(n);
+        }
+
     }
 }
