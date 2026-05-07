400b243aec4a844033d0d0ea82ba54dfdb714b39
Anton Lappa <lappaantony@gmail.com>
2026-05-07 16:40:28 +0200

    add sample code for recursion (#14)

---
+++ src/lesson_19_recursion/Main.java
@@ -0,0 +1,9 @@
+package lesson_19_recursion;
+
+public class Main {
+    static void main() {
+        int n = 4;
+        int factorial = Math.getFactorial(n);
+        System.out.println("Факторіал числа " + n + " є " + factorial);
+    }
+}
