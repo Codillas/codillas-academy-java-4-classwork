416dca68dd93971a5aadbe269b97d2d462a8765a
antonlappa <lappaantony@gmail.com>
2026-05-11 16:21:41 +0200

    structure changes

--- src/test/java/lesson_20_maven/EmployeeTest.java
+++ src/test/java/lesson_20_maven/EmployeeTest.java
@@ -1,11 +1,10 @@
-package test.lesson_20_maven;
+package lesson_20_maven;
 
-import lesson_20_maven.Employee;
+
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
