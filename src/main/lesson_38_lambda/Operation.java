0acac580a9e5314bc7d81895e906fd4342ea2a57
Anton Lappa <lappaantony@gmail.com>
2026-06-09 16:34:53 +0200

    add sample code for lambda (#28)

---
+++ src/main/lesson_38_lambda/Operation.java
@@ -0,0 +1,12 @@
+package main.lesson_38_lambda;
+
+import lombok.Data;
+
+@Data
+public class Operation implements Operationable {
+
+    @Override
+    public int calculate(int a, int b) {
+        return a +b;
+    }
+}
---
+++ src/main/lesson_38_lambda/Operationable.java
@@ -0,0 +1,5 @@
+package main.lesson_38_lambda;
+
+public interface Operationable {
+    int calculate(int a, int b);
+}
