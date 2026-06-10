43dea8e3d56c5740741c32d598b2650d9b774086
antonlappa <lappaantony@gmail.com>
2026-06-10 18:49:56 +0200

    add sample code for comparator and comparable

---
+++ src/main/java/lesson_40_comparable_and_comparator/BasketballPlayer.java
@@ -0,0 +1,28 @@
+package lesson_40_comparable_and_comparator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasketballPlayer implements Comparable<BasketballPlayer> {
+
+    private String name;
+    private int score;
+    private int age;
+
+
+    @Override
+    public int compareTo(BasketballPlayer otherPlayer) {
+
+        if (this.score == otherPlayer.score) {
+           return Integer.compare(this.age, otherPlayer.age);
+        }  else if (this.score < otherPlayer.score) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
